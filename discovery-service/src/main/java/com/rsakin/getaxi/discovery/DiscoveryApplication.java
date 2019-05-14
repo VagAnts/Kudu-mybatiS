a804e66952d1fc88ad0823246303382f155209b9
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-05-14 22:19:14 +0000

    Updated paket.bootstrapper.exe to address TLS 1.0 and 1.1 deprecation

--- discovery-service/src/main/java/com/rsakin/getaxi/discovery/DiscoveryApplication.java
+++ discovery-service/src/main/java/com/rsakin/getaxi/discovery/DiscoveryApplication.java
@@ -2,4 +2,6 @@ package com.rsakin.getaxi.discovery;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.bo
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.sprin
