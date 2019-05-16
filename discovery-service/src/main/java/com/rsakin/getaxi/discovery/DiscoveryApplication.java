90cc3b1edc7313c29f0e1db20b7531e3cc58e90f
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-05-16 09:23:24 +0000

    Initial unit tests

--- discovery-service/src/main/java/com/rsakin/getaxi/discovery/DiscoveryApplication.java
+++ discovery-service/src/main/java/com/rsakin/getaxi/discovery/DiscoveryApplication.java
@@ -4,4 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.sprin
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapp
