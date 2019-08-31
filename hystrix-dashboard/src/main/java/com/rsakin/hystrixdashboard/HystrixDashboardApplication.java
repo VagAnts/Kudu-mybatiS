d262d786ba35aec05be6a53d4286a64c854b404a
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-08-31 17:35:03 +0000

    Linux 3.0.40

--- hystrix-dashboard/src/main/java/com/rsakin/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/rsakin/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,4 +2,7 @@ package com.rsakin.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.clou
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootAppli
