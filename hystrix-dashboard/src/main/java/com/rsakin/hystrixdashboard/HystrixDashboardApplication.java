24e0807e09e0fe8019150ff58386486ffc889005
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-08-31 11:47:20 +0000

    updated parent-oss, common, api

--- hystrix-dashboard/src/main/java/com/rsakin/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/rsakin/hystrixdashboard/HystrixDashboardApplication.java
@@ -1,3 +1,5 @@
 package com.rsakin.hystrixdashboard;
 
-import org.springframework.boot
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.clou
