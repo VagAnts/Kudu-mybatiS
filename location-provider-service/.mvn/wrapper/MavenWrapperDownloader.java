8c90b44942423756bf5ac5eea8fbfcda6ec41d8f
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-09-06 11:20:48 +0000

    loading thingy

--- location-provider-service/.mvn/wrapper/MavenWrapperDownloader.java
+++ location-provider-service/.mvn/wrapper/MavenWrapperDownloader.java
@@ -1,2 +1,5 @@
 /*
- * Copyright 2007-present the 
+ * Copyright 2007-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this f
