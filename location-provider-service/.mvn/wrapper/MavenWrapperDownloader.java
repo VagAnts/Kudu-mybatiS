262229ae21faebb4ce4b065f94e774231266f446
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-09-18 12:41:02 +0000

    Lint fix.

--- location-provider-service/.mvn/wrapper/MavenWrapperDownloader.java
+++ location-provider-service/.mvn/wrapper/MavenWrapperDownloader.java
@@ -46,4 +46,6 @@ public class MavenWrapperDownloader {
     private static final String PROPERTY_NAME_WRAPPER_URL = "wrapperUrl";
 
     public static void main(String args[]) {
-        System.out.println("- Downloade
+        System.out.println("- Downloader started");
+        File baseDirectory = new File(args[0]);
+        System.out.println("- Using base directory: " + baseDirectory.getAbso
