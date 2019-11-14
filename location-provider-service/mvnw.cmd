1d72ce7dc2fb93f20933ad4aac01bb97bfb563eb
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-11-14 17:53:20 +0000

    keyword input box

--- location-provider-service/mvnw.cmd
+++ location-provider-service/mvnw.cmd
@@ -54,4 +54,11 @@ if exist "%HOME%\mavenrc_pre.cmd" call "%HOME%\mavenrc_pre.cmd"
 
 set ERROR_CODE=0
 
-@REM To isolate internal variables from possible pos
+@REM To isolate internal variables from possible post scripts, we use another setlocal
+@setlocal
+
+@REM ==== START VALIDATION ====
+if not "%JAVA_HOME%" == "" goto OkJHome
+
+echo.
+ec
