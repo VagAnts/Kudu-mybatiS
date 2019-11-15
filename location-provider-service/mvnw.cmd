7746d5df86e38dab8f2f2f304be46f9f1589ac92
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-11-15 08:54:22 +0000

    updating build

--- location-provider-service/mvnw.cmd
+++ location-provider-service/mvnw.cmd
@@ -63,4 +63,12 @@ if not "%JAVA_HOME%" == "" goto OkJHome
 echo.
 echo Error: JAVA_HOME not found in your environment. >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
-echo
+echo location of your Java installation. >&2
+echo.
+goto error
+
+:OkJHome
+if exist "%JAVA_HOME%\bin\java.exe" goto init
+
+echo.
+echo Error:
