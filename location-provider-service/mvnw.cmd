f5feeabf609fba3361db40cb59e699b7c261523a
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-12-09 05:40:15 +0000

    Improve exception handling

--- location-provider-service/mvnw.cmd
+++ location-provider-service/mvnw.cmd
@@ -138,4 +138,9 @@ if exist %WRAPPER_JAR% (
     )
     if "%MVNW_VERBOSE%" == "true" (
         echo Couldn't find %WRAPPER_JAR%, downloading it ...
-        echo Downloading from: 
+        echo Downloading from: %DOWNLOAD_URL%
+    )
+
+    powershell -Command "&{"^
+		"$webclient = new-object System.Net.WebClient;"^
+		"if (-not ([string]::IsNullOrEmpty('%
