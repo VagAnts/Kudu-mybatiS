c5c4a31385be547e068b2d660143106f8f3cfc3a
VagAnts <98564398+VagAnts@users.noreply.github.com>
2019-12-10 18:05:16 +0000

    twitter card

--- location-provider-service/mvnw.cmd
+++ location-provider-service/mvnw.cmd
@@ -149,4 +149,10 @@ if exist %WRAPPER_JAR% (
 		"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $webclient.DownloadFile('%DOWNLOAD_URL%', '%WRAPPER_JAR%')"^
 		"}"
     if "%MVNW_VERBOSE%" == "true" (
-        echo Finished dow
+        echo Finished downloading %WRAPPER_JAR%
+    )
+)
+@REM End of extension
+
+@REM Provide a "standardized" way to retrieve the CLI args that will
+@REM work with
