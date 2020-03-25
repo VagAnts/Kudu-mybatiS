49bf61722fce2a4e6e57800d07ed85e0a137a611
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-03-25 13:03:08 +0000

    Add module

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/ElasticClientService.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/ElasticClientService.java
@@ -4,4 +4,5 @@
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.data.elasticsearch.client.ClientConfiguration;
-//import org.springframework.d
+//import org.springframework.data.elasticsearch.client.RestClients;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchCo
