07bd1cbe7179a53739bb45e45c0999d230c4b8f3
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-03-25 07:30:17 +0000

    Fix download script compatibility with URL containing spaces

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
@@ -1,4 +1,9 @@
 package com.rsakin.getaxi.locationproviderservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.Sp
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationProviderServiceApplication {
+
+    public static void ma
