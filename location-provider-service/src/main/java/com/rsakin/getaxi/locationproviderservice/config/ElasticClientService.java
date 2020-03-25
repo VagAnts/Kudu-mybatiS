2e7dd70ea78edb1f3b943acdf8744a7dad1a3680
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-03-25 10:32:58 +0000

    MAINTAINERS (Write After Approval): Add myself.

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/ElasticClientService.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/ElasticClientService.java
@@ -1,1 +1,5 @@
-//package com.rsakin.getaxi.locationp
+//package com.rsakin.getaxi.locationproviderservice.config;
+//
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.springframework.context.annotation.Bean;
+//import 
