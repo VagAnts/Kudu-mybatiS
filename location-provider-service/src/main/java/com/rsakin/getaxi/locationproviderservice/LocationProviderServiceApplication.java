92bca9c4ffc38845315dac158cd2e41bfa28e469
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-03-25 08:15:18 +0000

    fix minor bug

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
@@ -6,4 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocationProviderServiceApplication {
 
-    public static void ma
+    public static void main(String[] args) {
+        SpringApplication.run(LocationProviderServiceApplication.class, args);
+    }
+
+}
