ec5858f6b510e32aa30524b5ee449fb04421f321
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-03-31 12:46:23 +0000

    fix broken import

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/KafkaConsumerConfig.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/KafkaConsumerConfig.java
@@ -1,3 +1,5 @@
 package com.rsakin.getaxi.locationproviderservice.config;
 
-import com.rsakin.g
+import com.rsakin.getaxi.locationproviderservice.model.LocationDTO;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.ap
