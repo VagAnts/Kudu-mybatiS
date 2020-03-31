ea35726f8a40994e9682e776e37ff73c21c152d8
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-03-31 15:44:31 +0000

    adding should_enlarge_memory_pool to polite garbage collector

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/KafkaConsumerConfig.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/config/KafkaConsumerConfig.java
@@ -2,4 +2,6 @@ package com.rsakin.getaxi.locationproviderservice.config;
 
 import com.rsakin.getaxi.locationproviderservice.model.LocationDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.ap
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.spring
