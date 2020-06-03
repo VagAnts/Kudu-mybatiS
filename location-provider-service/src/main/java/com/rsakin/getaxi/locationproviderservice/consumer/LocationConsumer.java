4bf752a8e8e0b449515cff2993329ba46509dc1a
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-06-03 10:55:26 +0000

    added program

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/consumer/LocationConsumer.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/consumer/LocationConsumer.java
@@ -3,4 +3,7 @@ package com.rsakin.getaxi.locationproviderservice.consumer;
 import com.rsakin.getaxi.locationproviderservice.exception.LocationNotFoundException;
 import com.rsakin.getaxi.locationproviderservice.model.Location;
 import com.rsakin.getaxi.locationproviderservice.service.LocationService;
-import lombok.RequiredArg
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Ser
