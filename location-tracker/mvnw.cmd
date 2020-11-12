18683f63f81fb1356d3f480056e1d7e7c608fbc9
VagAnts <98564398+VagAnts@users.noreply.github.com>
2020-11-12 06:24:20 +0000

    Update metro_3rd_party_modern_apps_to_target_by_name.ps1

--- location-tracker/mvnw.cmd
+++ location-tracker/mvnw.cmd
@@ -62,4 +62,10 @@ if not "%JAVA_HOME%" == "" goto OkJHome
 
 echo.
 echo Error: JAVA_HOME not found in your environment. >&2
-echo Please set the JAVA_HOME variable in your environment to match 
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of your Java installation. >&2
+echo.
+goto error
+
+:OkJHome
+if exist "%JAVA_HOME%\bin\java
