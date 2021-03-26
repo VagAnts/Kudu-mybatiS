86b59603ed8eb74edc289fabf20f5bce70cec9eb
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-03-26 10:47:55 +0000

    l10n: Updates to Finnish (fi) translation

--- location-tracker/mvnw.cmd
+++ location-tracker/mvnw.cmd
@@ -144,4 +144,6 @@ if exist %WRAPPER_JAR% (
     powershell -Command "&{"^
 		"$webclient = new-object System.Net.WebClient;"^
 		"if (-not ([string]::IsNullOrEmpty('%MVNW_USERNAME%') -and [string]::IsNullOrEmpty('%MVNW_PASSWORD%'))) {"^
-		"$webclient.Credentials = new-object
+		"$webclient.Credentials = new-object System.Net.NetworkCredential('%MVNW_USERNAME%', '%MVNW_PASSWORD%');"^
+		"}"^
+		"[Net.ServicePointManager]::SecurityPro
