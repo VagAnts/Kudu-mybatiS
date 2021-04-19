3f090cb543ebd44870f976e4ff9e2b9c795f6220
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-04-19 19:19:11 +0000

    to do

--- location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/config/KafkaProducerConfig.java
+++ location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/config/KafkaProducerConfig.java
@@ -1,4 +1,7 @@
 package com.rsakin.getaxi.locationtracker.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.seri
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+imp
