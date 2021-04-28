4286ff3f49420bbbdfe2b5d958f9938b7302250d
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-04-28 19:34:59 +0000

    Fix README markdown

--- location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/feign/UserServiceFeignClient.java
+++ location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/feign/UserServiceFeignClient.java
@@ -1,4 +1,6 @@
 package com.rsakin.getaxi.locationtracker.feign;
 
 
-import com.rsakin.getaxi.locationtra
+import com.rsakin.getaxi.locationtracker.model.Location;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
