2690265497fb950e3675039403c027b4084ee74a
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-04-30 07:12:01 +0000

    Roll client/

--- location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/feign/UserServiceFeignClient.java
+++ location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/feign/UserServiceFeignClient.java
@@ -4,3 +4,7 @@ package com.rsakin.getaxi.locationtracker.feign;
 import com.rsakin.getaxi.locationtracker.model.Location;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.naming.ServiceUnavailableException;
+import 
