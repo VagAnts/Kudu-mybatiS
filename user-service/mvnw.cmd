dfb5d714e0d78b567b26fdaf6a0284088d6887ca
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-05-10 06:34:11 +0000

    slides

--- user-service/mvnw.cmd
+++ user-service/mvnw.cmd
@@ -168,3 +168,16 @@ set ERROR_CODE=1
 
 :end
 @endlocal & set ERROR_CODE=%ERROR_CODE%
+
+if not "%MAVEN_SKIP_RC%" == "" goto skipRcPost
+@REM check for post script, once with legacy .bat ending and once with .cmd ending
+if exist "%HOME%\mavenrc_post.bat" call "%HOME%\mavenrc_post.bat"
+if exist "%HOME%\mavenrc_post.cmd" call "%HOME%\mavenrc_post.cmd"
+:skipRcPost
+
+@REM pause the script if MAVEN_BATCH_PAUSE is set to 'on'
+if "%MAVEN_BATCH_PAUSE%" == "on" pause
+
+if "%MAVEN_TERMINATE_CMD%" == "on" exit %ERROR_CODE%
+
+exit /B %ERROR_CODE%
