69a156d19b980cba593ba3b9f074f86211de8f35
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-09-12 06:57:20 +0000

    Fixes build.

--- user-service/src/main/java/com/rsakin/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/rsakin/userservice/UserServiceApplication.java
@@ -1,3 +1,5 @@
 package com.rsakin.userservice;
 
-import org.springframework.boot
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotatio
