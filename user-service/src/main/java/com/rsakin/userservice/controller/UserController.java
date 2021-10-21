f4cefcc664bb207b8c8dc768eda16147c80f32ac
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-10-21 13:01:20 +0000

    changed cname

--- user-service/src/main/java/com/rsakin/userservice/controller/UserController.java
+++ user-service/src/main/java/com/rsakin/userservice/controller/UserController.java
@@ -3,4 +3,8 @@ package com.rsakin.userservice.controller;
 import com.rsakin.userservice.dto.UserDTO;
 import com.rsakin.userservice.entity.User;
 import com.rsakin.userservice.model.Location;
-import com.rsakin.userservice.service.UserS
+import com.rsakin.userservice.service.UserService;
+import com.rsakin.userservice.simu.LocationSimulator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import o
