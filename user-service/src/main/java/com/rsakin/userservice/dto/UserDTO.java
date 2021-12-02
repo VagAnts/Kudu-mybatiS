8be944d7d2d9175374ef487be7d9ecd424d96848
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-12-02 16:51:36 +0000

    Update install.sh

--- user-service/src/main/java/com/rsakin/userservice/dto/UserDTO.java
+++ user-service/src/main/java/com/rsakin/userservice/dto/UserDTO.java
@@ -2,4 +2,12 @@ package com.rsakin.userservice.dto;
 
 import com.rsakin.userservice.entity.Address;
 import com.rsakin.userservice.entity.UserRole;
-import lombok.
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Bu
