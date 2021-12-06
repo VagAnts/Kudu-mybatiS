12d34cd83cfe12bb3af14a32ee205c325af16e28
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-12-06 20:20:14 +0000

    Updating _data/api-commons/smartbear-software/apis.yaml via Laneworks CMS Publish

--- user-service/src/main/java/com/rsakin/userservice/entity/Address.java
+++ user-service/src/main/java/com/rsakin/userservice/entity/Address.java
@@ -3,3 +3,10 @@ package com.rsakin.userservice.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
