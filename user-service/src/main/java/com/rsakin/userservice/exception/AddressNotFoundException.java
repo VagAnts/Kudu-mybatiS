257941863b154ce6bc6c97975fd0b55d675955a9
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-12-20 10:00:38 +0000

    Fixes #58

--- user-service/src/main/java/com/rsakin/userservice/exception/AddressNotFoundException.java
+++ user-service/src/main/java/com/rsakin/userservice/exception/AddressNotFoundException.java
@@ -1,3 +1,7 @@
 package com.rsakin.userservice.exception;
 
-public class AddressNotFoundException extends NotFoundExcepti
+public class AddressNotFoundException extends NotFoundException {
+    public AddressNotFoundException(Integer id) {
+        super("Address not found by id : " + id);
+    }
+}
