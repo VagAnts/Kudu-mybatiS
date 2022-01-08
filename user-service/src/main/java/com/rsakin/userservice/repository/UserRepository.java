e439faeae7378f53aa1c106bf6e96346f1bc777a
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-01-08 16:48:37 +0000

    Ignore some files.

--- user-service/src/main/java/com/rsakin/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/rsakin/userservice/repository/UserRepository.java
@@ -4,4 +4,9 @@ import com.rsakin.userservice.entity.Address;
 import com.rsakin.userservice.entity.User;
 import com.rsakin.userservice.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
-import 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository e
