e86ed8a27f3f9d99f2b38f824e883951de1edbd6
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-01-15 13:01:37 +0000

    [blacksmith] Bump version to 0.1.1

--- user-service/src/main/java/com/rsakin/userservice/simu/LocationSimulator.java
+++ user-service/src/main/java/com/rsakin/userservice/simu/LocationSimulator.java
@@ -2,4 +2,7 @@ package com.rsakin.userservice.simu;
 
 import com.rsakin.userservice.entity.User;
 import com.rsakin.userservice.model.Location;
-import com.rsakin.userser
+import com.rsakin.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.ann
