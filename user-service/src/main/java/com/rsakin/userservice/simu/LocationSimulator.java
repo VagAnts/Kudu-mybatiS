72247335f7f4bc94177d8c99481066a16bb6dfec
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-01-17 18:01:21 +0000

    Add localversion-wireless to identify builds from this tree.

--- user-service/src/main/java/com/rsakin/userservice/simu/LocationSimulator.java
+++ user-service/src/main/java/com/rsakin/userservice/simu/LocationSimulator.java
@@ -5,4 +5,9 @@ import com.rsakin.userservice.model.Location;
 import com.rsakin.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.ann
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.co
