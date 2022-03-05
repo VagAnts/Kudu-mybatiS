61498779981c17f5a75d21ccd940d4a9455c8461
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-03-05 06:26:05 +0000

    time: Avoid making adjustments if we haven't accumulated anything

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -1,4 +1,11 @@
 -- Table Schemas
 DROP TABLE IF EXISTS users;
 
-DROP TABLE IF EXISTS addr
+DROP TABLE IF EXISTS address;
+
+CREATE TABLE address(
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    city VARCHAR(15) NOT NULL,
+    street VARCHAR(15),
+    building_no INT,
+   
