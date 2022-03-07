360296bd753dde95589f6d4c1a90195f013f9a80
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-03-07 21:35:23 +0000

    Update readme instructions

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -15,4 +15,7 @@ CREATE TABLE users(
     id INT AUTO_INCREMENT  PRIMARY KEY,
     name VARCHAR(20),
     lastname VARCHAR(20),
-    username VARC
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(20) UNIQUE,
+    password varchar(100) NOT NULL,
+    role varchar(15) NOT NULL
