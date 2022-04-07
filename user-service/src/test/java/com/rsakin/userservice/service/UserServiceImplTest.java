995f185e48cfafbf2dd5f5f41ba51780c099e208
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-04-07 19:14:55 +0000

    Create chat.lua

--- user-service/src/test/java/com/rsakin/userservice/service/UserServiceImplTest.java
+++ user-service/src/test/java/com/rsakin/userservice/service/UserServiceImplTest.java
@@ -58,3 +58,89 @@ public class UserServiceImplTest {
         assertEquals("lastname0", userDTOList.get(0).getLastname());
 
     }
+
+    @Test
+    public void should_getOneUser() {
+        // stub
+        User sampleUser = getSampleUserList(1).get(0);
+
+        // when
+        Mockito.when(userRepository.findById(sampleUser.getId())).thenReturn(java.util.Optional.of(sampleUser));
+
+        // then
+        UserDTO one = userService.getOne(sampleUser.getId());
+
+        assertEquals("name0", one.getName());
+        assertEquals("lastname0", one.getLastname());
+
+    }
+
+    @Test
+    public void should_createOneUser() {
+        // stub
+        User sampleUser = getSampleUserList(1).get(0);
+
+        // when
+        Mockito.when(userRepository.save(any(User.class))).thenReturn(sampleUser);
+
+        // then
+        UserDTO one = userService.addOne(sampleUser);
+
+        assertEquals(sampleUser.getName(), one.getName());
+        assertEquals(sampleUser.getLastname(), one.getLastname());
+        assertEquals(sampleUser.getEmail(), one.getEmail());
+
+    }
+
+    @Test
+    public void should_NOT_updateUser() {
+        // stub
+        User user = getSampleUserList(1).get(0);
+
+        // then
+        UserNotFoundException userNotFoundException = assertThrows(
+                UserNotFoundException.class,
+                () -> userService.updateOne(user),
+                "Expected doThing() to throw, but it didn't"
+        );
+
+        assertTrue(userNotFoundException.getMessage().contains("User not found"));
+    }
+
+    @Test
+    public void should_deleteUser() {
+        // stub
+        User user = getSampleUserList(1).get(0);
+
+        // when
+        Mockito.when(userRepository.findById(user.getId()))
+                .thenReturn(java.util.Optional.of(user));
+        // doNothing().when(userRepository).delete(user);
+
+        UserDTO one = userService.getOne(user.getId());
+        addressService.deleteOne(one.getId());
+
+        assertNull(addressService.getOne(user.getId()));
+    }
+
+    private List<User> getSampleUserList(int number) {
+        List<User> userList = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            userList.add(getSampleUser(i, "name" + i,
+                    "lastname" + i, "user" + i,
+                    "mail" + i + "@com"));
+        }
+        return userList;
+    }
+
+    private User getSampleUser(int number, String name, String last, String username, String email) {
+        return User.builder()
+                .id(number)
+                .name(name)
+                .lastname(last)
+                .username(username)
+                .email(email)
+                .build();
+    }
+
+}
