3b3921fef200e282b00934f28737333c8b464fff
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-05-25 20:48:52 +0000

    reduce logging

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
@@ -1,1 +1,5 @@
-package com.rsakin.getaxi.proxy
+package com.rsakin.getaxi.proxy.config;
+
+import com.rsakin.getaxi.proxy.filter.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import o
