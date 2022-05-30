18eedc08e9eef518776c1be05d427942435f8be7
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-05-30 05:37:13 +0000

    more clear

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
@@ -4,4 +4,6 @@ import com.rsakin.getaxi.proxy.filter.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import or
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.s
