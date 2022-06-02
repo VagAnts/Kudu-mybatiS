d6dfe8ba0d1b546d4f0a390ddc239b9b3e9eb0e0
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-06-02 05:56:20 +0000

    More of the same.

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
@@ -7,4 +7,5 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.co
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.Ht
