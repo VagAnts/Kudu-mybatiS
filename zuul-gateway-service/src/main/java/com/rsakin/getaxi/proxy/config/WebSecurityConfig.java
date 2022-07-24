d274f287b6ea5a4b9eb782baad83a1f614f25f1d
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-07-24 11:10:18 +0000

    Update Rome_1.txt

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
@@ -68,4 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // enable auth apis
                 .antMatchers("/authenticate", "/register", "/hello").permitAll()
                 // all other requests need to be authenticated
-                .a
+                .anyRequest().authenticated()
+                .and()
+                // make sure we use stateless session; session won't be used to
+  
