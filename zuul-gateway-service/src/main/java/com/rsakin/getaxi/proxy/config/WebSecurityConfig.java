096085b72deeabc8ed6a3f1df7fc1e7cce49e5bc
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-10-12 11:16:27 +0000

    smal fix

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/config/WebSecurityConfig.java
@@ -73,3 +73,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Add a filt
