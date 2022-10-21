797183da9a4cbc227d3b17cc1170140135e6ab10
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-10-21 12:21:26 +0000

    Bump version to 0.18.0

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/dao/feign/UserServiceFeign.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/dao/feign/UserServiceFeign.java
@@ -1,3 +1,6 @@
 package com.rsakin.getaxi.proxy.dao.feign;
 
-import com.rsakin.getaxi.proxy.dao.fallback.
+import com.rsakin.getaxi.proxy.dao.fallback.UserServiceFeignFallback;
+import com.rsakin.getaxi.proxy.dao.model.User;
+import com.rsakin.getaxi.proxy.dao.model.UserDTO;
+imp
