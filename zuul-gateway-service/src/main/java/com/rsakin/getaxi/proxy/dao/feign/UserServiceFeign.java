3575582054ed6b8985f9276e391cd60161020355
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-10-23 07:37:53 +0000

    test doc

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/dao/feign/UserServiceFeign.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/dao/feign/UserServiceFeign.java
@@ -5,4 +5,6 @@ import com.rsakin.getaxi.proxy.dao.model.User;
 import com.rsakin.getaxi.proxy.dao.model.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.w
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.
