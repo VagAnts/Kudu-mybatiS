9142f44ad9610d47955e2487afa75b6b97ed3938
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-10-29 18:24:43 +0000

    Fix shellcheck

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/JwtRequestFilter.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/JwtRequestFilter.java
@@ -1,3 +1,6 @@
 package com.rsakin.getaxi.proxy.filter;
 
-import com.rsakin.getaxi.pr
+import com.rsakin.getaxi.proxy.service.JwtUserDetailsService;
+import com.rsakin.getaxi.proxy.util.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombo
