d79344c96e0ebe7e420921b90e27c6a3b890a650
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-11-01 06:49:29 +0000

    huge refactor

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/JwtRequestFilter.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/JwtRequestFilter.java
@@ -5,4 +5,6 @@ import com.rsakin.getaxi.proxy.util.JwtTokenUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.Userna
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org
