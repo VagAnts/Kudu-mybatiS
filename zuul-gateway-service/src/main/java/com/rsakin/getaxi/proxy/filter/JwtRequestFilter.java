daebf2f69a605ba649cee2b4080b366aa872d869
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-11-19 09:56:43 +0000

    adjusting readme

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/JwtRequestFilter.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/JwtRequestFilter.java
@@ -39,4 +39,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         // JWT Token is in the form "Bearer token". Remove Bearer word and get
         // only the Token
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-            jwt
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                username = jwtTokenUtil.getUsernameFromToken(jwtTok
