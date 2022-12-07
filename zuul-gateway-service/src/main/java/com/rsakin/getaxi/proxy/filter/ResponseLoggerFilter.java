9bb805e3cd34f58b4d97c946bf4142330b5fa765
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-12-07 07:33:00 +0000

    Fix paths

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/ResponseLoggerFilter.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/filter/ResponseLoggerFilter.java
@@ -3,4 +3,9 @@ package com.rsakin.getaxi.proxy.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.rsakin.getaxi.proxy.util.FilterUtil;
-i
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ResponseLoggerFilter ext
