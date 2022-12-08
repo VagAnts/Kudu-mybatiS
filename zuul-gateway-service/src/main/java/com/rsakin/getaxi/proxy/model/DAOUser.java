d80e11ee42571c29ad35e0b6ad46ffe32dd200f1
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-12-08 20:17:29 +0000

    Added travis integration

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/model/DAOUser.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/model/DAOUser.java
@@ -1,3 +1,10 @@
 package com.rsakin.getaxi.proxy.model;
 
-im
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DAOUser 
