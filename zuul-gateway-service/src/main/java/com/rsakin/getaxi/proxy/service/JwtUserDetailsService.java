4eace2280d5d2ee255ca648e8a90c061d6cc6fb2
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-12-13 08:06:49 +0000

    Fixed stuff.

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
@@ -3,4 +3,6 @@ package com.rsakin.getaxi.proxy.service;
 import com.rsakin.getaxi.proxy.dao.feign.UserServiceFeign;
 import com.rsakin.getaxi.proxy.dao.model.User;
 import com.rsakin.getaxi.proxy.dao.model.UserDTO;
-import org.springfram
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframew
