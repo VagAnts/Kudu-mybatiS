74c2790e2e18a7b9e2072950b988d5461f36e7ea
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-12-15 18:13:57 +0000

    Fixing compiler warnings

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
@@ -5,4 +5,6 @@ import com.rsakin.getaxi.proxy.dao.model.User;
 import com.rsakin.getaxi.proxy.dao.model.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframew
