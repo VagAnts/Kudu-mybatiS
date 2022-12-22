e9359d25719c0ca16f6228c50460b170f060b332
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-12-22 18:36:35 +0000

    contrib

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
@@ -4,4 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+imp
