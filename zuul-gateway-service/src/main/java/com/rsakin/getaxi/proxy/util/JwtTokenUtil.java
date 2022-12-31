b67864e2647f9a1cd1155287bab86f6fe998f736
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-12-31 06:16:06 +0000

    A test

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
@@ -12,4 +12,10 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Functio
+import java.util.function.Function;
+
+@RefreshScope
+@Component
+public class JwtTokenUtil implements Serializable {
+
+    private static final long serialVersionUID = -255018516
