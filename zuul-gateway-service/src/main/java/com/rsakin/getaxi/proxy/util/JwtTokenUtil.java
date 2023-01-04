03a4e4de1ad809cd9cdb55ef2916c057ef3a1f98
VagAnts <98564398+VagAnts@users.noreply.github.com>
2023-01-04 19:26:41 +0000

    moved images

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
@@ -35,4 +35,6 @@ public class JwtTokenUtil implements Serializable {
         return getClaimFromToken(token, Claims::getExpiration);
     }
 
-    public <T> T getClaimFromToken(String t
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(cla
