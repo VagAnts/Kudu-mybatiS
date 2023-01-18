dae8f668677bac028fd09a6289aad49da1a0633b
VagAnts <98564398+VagAnts@users.noreply.github.com>
2023-01-18 08:24:44 +0000

    Adding scripts

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/JwtTokenUtil.java
@@ -54,4 +54,9 @@ public class JwtTokenUtil implements Serializable {
     // generate token for user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        return doGenerateToken(claims, userDetails.getUsername
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+
+    // while creating the token -
+    // 1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
+    // 2. Sign the 
