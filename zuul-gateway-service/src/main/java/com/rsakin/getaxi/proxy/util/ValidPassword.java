8d2666ca552c917ead601609b0f11be8fa19be33
VagAnts <98564398+VagAnts@users.noreply.github.com>
2023-02-05 20:59:23 +0000

    “upload”

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/ValidPassword.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/util/ValidPassword.java
@@ -8,4 +8,9 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.R
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target({FIELD, ANNOTATION_TYPE})
+@Retention
